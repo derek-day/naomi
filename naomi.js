5aae7f88c54410beddbcf63ffd3df4f93d1fdc6e
Derek Day <derek.andrew.day@gmail.com>
2023-09-09 02:08:25 -0400

    change js func

--- naomi.js
+++ naomi.js
@@ -3,6 +3,6 @@ function gridImageSize() {
     $('.about-image').height($('.about-text-container').height())
 }
 
-gridImageSize();
-
 window.addEventListener('resize', gridImageSize);
+
+gridImageSize();
