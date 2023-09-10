5c2ecb83334a4f3ba9a1a3e20c2ce1c16fe88276
Derek Day <derek.andrew.day@gmail.com>
2023-09-09 21:01:06 -0400

    no image height update on mobile

--- naomi.js
+++ naomi.js
@@ -1,8 +1,18 @@
-
-function gridImageSize() {
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+    
+if (!isMobile()) {
+    //place script you don't want to run on mobile here
     $('.about-image').height($('.about-text-container').height())
+    window.addEventListener('resize', gridImageSize);
+    gridImageSize();
 }
 
-window.addEventListener('resize', gridImageSize);
+// function gridImageSize() {
+//     $('.about-image').height($('.about-text-container').height())
+// }
 
-gridImageSize();
+// window.addEventListener('resize', gridImageSize);
+
+// gridImageSize();
