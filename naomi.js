fcd64ebaf9aaba8915cf01f7f34dbd5b06b2483e
Derek Day <derek.andrew.day@gmail.com>
2023-09-10 02:40:31 -0400

    about and library update

--- naomi.js
+++ naomi.js
@@ -1,17 +1,21 @@
 function isMobile() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
+
+function gridImageSize() {
+    // $('.about-image').height($('.about-text-container').height())
+    // $('.library-image').height($('.library-text-container').height()+130+"px")
+    $('.about-image-container').height($('.about-text-container').height())
+    $('.library-image-container').height($('.library-text-container').height()+100+"px")
+}
     
 if (!isMobile()) {
+// if ($(window).width() > 1000) {
+    // console.log($(window).width());
     //place script you don't want to run on mobile here
-    $('.about-image').height($('.about-text-container').height())
     window.addEventListener('resize', gridImageSize);
     gridImageSize();
 }
-
-// function gridImageSize() {
-//     $('.about-image').height($('.about-text-container').height())
-// }
 
 // window.addEventListener('resize', gridImageSize);
 
