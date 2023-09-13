9ad37f101462b4efded1571a67c0d1112512f6f2
Derek Day <derek.andrew.day@gmail.com>
2023-09-12 23:52:33 -0400

    fix library grid

--- naomi.js
+++ naomi.js
@@ -3,23 +3,14 @@ function isMobile() {
 }
 
 function gridImageSize() {
-    // $('.about-image').height($('.about-text-container').height())
     $('.library-image').height($('.library-text-container').height()+150+"px")
     $('.about-image-container').height($('.about-text-container').height())
-    // $('.library-image-container').height($('.library-text-container').height()+100+"px")
 
-
-    // var libraryWidth = $('.library-image').width()
-
-    // var libraryColumn = "1fr " + libraryWidth + "px"
-
-    // document.getElementById("library").style.gridTemplateColumns = libraryColumn
+    $(".library-text-header").width($(".library-p").width())
+    $(".library-button").width($(".library-p").width())
 }
     
 if (!isMobile()) {
-// if ($(window).width() > 1000) {
-    // console.log($(window).width());
-    //place script you don't want to run on mobile here
     window.addEventListener('resize', gridImageSize);
     gridImageSize();
 }
