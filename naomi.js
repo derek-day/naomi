cfa53b5f4b2fb5e99c29efb59ca81cfdd4334cd3
Derek Day <derek.andrew.day@gmail.com>
2023-09-13 16:01:39 -0400

    social and footer media buttons

--- naomi.js
+++ naomi.js
@@ -28,24 +28,9 @@ $(window).on('scroll', function() {
   }
 });
 
-function elementsOverlap(el1, el2) {
-  const domRect1 = el1.getBoundingClientRect();
-  const domRect2 = el2.getBoundingClientRect();
-
-  return !(
-    domRect1.top > domRect2.bottom ||
-    domRect1.right < domRect2.left ||
-    domRect1.bottom < domRect2.top ||
-    domRect1.left > domRect2.right
-  );
-}
-
-const el1 = document.getElementById('social-main');
-const el2 = document.getElementById('social-h1');
-
-console.log(elementsOverlap(el1, el2)); // 👉️ true
-
-
-if (elementsOverlap(el1, el2)) {
-  // el2.style.color="white"; works but changes entire word
-}
+// var rect = element.getBoundingClientRect();
+const socialmain = document.getElementById('social-main');
+var rect = socialmain.getBoundingClientRect();
+console.log(rect.top, rect.right, rect.bottom, rect.left);
+const socialh1 = document.getElementById('social-h1');
+// socialh1.style.left = '100px';
